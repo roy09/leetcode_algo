423633491f69bcbde02e798e5e9343c21b013297
Jyotirmoy Roy <jroy2516@uni.sydney.edu.au>
2017-10-28 17:10:58 +1100

    sol for algo #1 TwoSums

--- src/TwoSum_1.java
+++ src/TwoSum_1.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum_1 {
 
 
     public static void main(String[] args){
         int[] nums = {2, 4, 10};
-        int[] answer = new TwoSum_1().twoSum(nums, 15);
+        int[] answer = new TwoSum_1().twoSum(nums, 14);
 
         if (answer != null){
             System.out.println("index 0 : " + answer[0]);
@@ -14,14 +17,18 @@ public class TwoSum_1 {
     }
 
     private int[] twoSum(int[] nums, int target) {
-        for(int c1 = 0; c1 < nums.length; c1++){
-            for(int c2= c1 + 1; c2 < nums.length; c2++){
-                if(nums[c1] + nums[c2] == target){
-                    int[] data = {c1, c2};
-                    return data;
-                }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int count = 0; count < nums.length; count++){
+            map.put(nums[count], count);
+        }
+
+        for (int count = 0; count < nums.length; count++){
+            int complement = target - nums[count];
+            if (map.containsKey(complement) && map.get(complement) != count){
+                return new int[] {count, map.get(complement)};
             }
         }
-        return null;
+        throw new IllegalArgumentException("No solution for you");
     }
 }
